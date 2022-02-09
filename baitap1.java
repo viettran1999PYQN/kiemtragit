4733503ca415712e22ef209a01c32d1aad49c796
unknown <viettranquoc24111999@gmail.com>
2022-02-09 09:05:15 -0800

    kiem tra giua ky

---
+++ baitap1.java
@@ -0,0 +1,33 @@
+package IT;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class baitap1 {
+
+	public static void main(String[] args) {
+		ArrayList<E>
+		
+	    Integer a=1;
+	    Integer b=1;
+	    System.out.println(System.identityHas
+	    		hCode(a));
+	    System.out.println(System.identityHashCode(b));
+	    
+		
+				
+				;
+
+			
+
+	
+	}
+
+	
+
+}
